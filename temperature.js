ee13eec6d1aeee1fe78609945abebbe33bd5e68f
Makoa <32736507+santarini@users.noreply.github.com>
2018-02-13 16:05:28 -1000

    Create temperature.js

---
+++ temperature.js
@@ -0,0 +1,9 @@
+function tempConvert(farenheit){
+				return (5/9) * (farenheit-32);
+			}
+      
+var y = 77;
+var x = tempConvert(y);
+var text = "The temperature in Farenheit is " + y + ", the temperature in celsius is " + x;
+
+console.log(text);
