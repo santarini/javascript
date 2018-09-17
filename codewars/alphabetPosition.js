861e1adec1b5d4c7373863acb5f7877cbeb45cf7
Makoa <32736507+santarini@users.noreply.github.com>
2018-09-17 00:52:37 -1000

    Update alphabetPosition.js

--- codewars/alphabetPosition.js
+++ codewars/alphabetPosition.js
@@ -2,10 +2,10 @@ function alphabetPosition(text) {
   var finalString = "";
   var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   for(x in text){
-    if (alphabet.indexOf(text.charAt(x)) === -1){
+    if (alphabet.indexOf(text.charAt(x).toLowerCase()) == -1){
       continue;
     }else{
-   newString = (alphabet.indexOf(text.charAt(x.toLowerCase()))+1) + " ";
+   newString = (alphabet.indexOf(text.charAt(x).toLowerCase())+1) + " ";
     }
     finalString = finalString + newString;
   }
