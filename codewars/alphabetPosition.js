e81388a7fc160d629ddfbe5ae044e742adec379f
Makoa <32736507+santarini@users.noreply.github.com>
2018-09-17 01:47:34 -1000

    Update alphabetPosition.js

--- codewars/alphabetPosition.js
+++ codewars/alphabetPosition.js
@@ -1,17 +1,14 @@
 function alphabetPosition(text) {
-  var finalString = "";
+  var positionList = [];
   var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   for(x in text){
     if (alphabet.indexOf(text.charAt(x).toLowerCase()) == -1){
       continue;
-    }if(x == 0){
-    newString = (alphabet.indexOf(text.charAt(x).toLowerCase())+1);
     }else{
-   newString = " " + (alphabet.indexOf(text.charAt(x).toLowerCase())+1); 
+    positionList.push(alphabet.indexOf(text.charAt(x).toLowerCase())+1);
     }
-    finalString = finalString + newString;
-  }
-  return(finalString);
+   }
+  alert(positionList.join(' '));
 }
 
 alphabetPosition("The sunset sets at twelve o' clock.");
