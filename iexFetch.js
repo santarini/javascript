307cbae50b9869c88f32ab9bf9d14952d8f6502d
Makoa <32736507+santarini@users.noreply.github.com>
2018-09-16 23:29:53 -1000

    Create iexFetch.js

---
+++ iexFetch.js
@@ -0,0 +1,7 @@
+fetch('https://api.iextrading.com/1.0/stock/aapl/financials')
+.then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+  console.log(JSON.stringify(myJson.financials[0].reportDate));
+  });
