a880db957b14fcc66fdb4896086e99c01409e08a
Makoa <32736507+santarini@users.noreply.github.com>
2018-09-17 00:34:55 -1000

    Create alphabetPosition.js

---
+++ codewars/alphabetPosition.js
@@ -0,0 +1,8 @@
+function alphabetPosition(text) {
+  var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  for(x in text){
+   alert(alphabet.indexOf(text.charAt(x)));
+  }
+}
+
+alphabetPosition('bac');
