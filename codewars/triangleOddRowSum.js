383d79c34fc05bd7923dea9add92547af037fcea
Makoa <32736507+santarini@users.noreply.github.com>
2018-09-16 20:54:32 -1000

    Update triangleOddRowSum.js

--- codewars/triangleOddRowSum.js
+++ codewars/triangleOddRowSum.js
@@ -1,3 +1,11 @@
 function triangleOddRowSum(n){
-alert(Math.pow((n-1),2)+n);
+  y = Math.pow((n-1),2)+n;
+  //alert(y);
+  z = 0;
+  for (i = 1; i < n; i++){
+    x = y + (i*2);
+    //alert(x);
+    z = z + x;    
+  }
+  alert(z + y)
 }
