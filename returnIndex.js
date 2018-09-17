7b3d44cd2cbc9e81a8c5e62c489f4307a2eedd75
Makoa <32736507+santarini@users.noreply.github.com>
2018-09-17 00:11:55 -1000

    Create returnIndex.js

---
+++ returnIndex.js
@@ -0,0 +1,5 @@
+function returnIndex(array) {
+  return('found the needle at position ' + haystack.indexOf('needle'));
+}
+
+returnIndex(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk'])
