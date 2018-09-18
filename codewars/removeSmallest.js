aae5d15bf9b5ed67049add5a266c4a16f731f8a5
Makoa <32736507+santarini@users.noreply.github.com>
2018-09-18 12:41:03 -1000

    Update removeSmallest.js

--- codewars/removeSmallest.js
+++ codewars/removeSmallest.js
@@ -3,12 +3,8 @@ function removeSmallest(numbers) {
   x = Math.min.apply(null, numbers);
   y = numbers.indexOf(x);
   count = 0;
-  for (z in numbers){
-    if (z == x){
-      count +=1;
-    }
-  }
-  numbers.splice(1, y);  
+
+  numbers.splice(y, 1);  
   console.log(numbers);
 }
 
