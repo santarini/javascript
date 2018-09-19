6319e110c85b97b42b875b7eb0b3647502c045f0
Makoa <32736507+santarini@users.noreply.github.com>
2018-09-18 15:21:58 -1000

    Update alphabetPosition.js

--- codewars/alphabetPosition.js
+++ codewars/alphabetPosition.js
@@ -14,4 +14,21 @@ function removeSmallest(numbers) {
   return numbers
 }
 
+function removeSmallest(numbers) {
+  x = Math.min.apply(null, numbers);
+  y = numbers.indexOf(x);
+  indexCount = [];
+  for (z in numbers){
+    if (numbers[z] == x){
+      indexCount.push(Number(z));
+    }
+  }
+  if(indexCount.length > 1){
+     y = Math.min.apply(null, indexCount);
+  }
+  return(numbers.filter(function(item){
+    return item != numbers[y];
+  }));
+}
+
 removeSmallest([1,2,1,4,5,1])
